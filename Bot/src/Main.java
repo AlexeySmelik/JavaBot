d45e507080a0958c3d0b770694efed4ccee38c2d
a_lesh_a <diadool@list.ru>
2021-09-18 13:33:43 +0500

    Added helper for questions, with which we can get question-answer object. These questions have to written down in the Bot/src/res/questions.txt. Format: 'Question?'-'Answer'.

--- Bot/src/Main.java
+++ Bot/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String [] args){
-        System.out.println("Test!");
+        var q = new QuestionHelper();
+        for (var i = 0; i < 3; i++) {
+            var form = q.getRandomQuestion();
+            System.out.println(String.format("Вопрос: %s \nCorrect answer: %s\n", form.question, form.answer));
+        }
     }
 }
---
+++ Bot/src/QuestionForm.java
@@ -0,0 +1,9 @@
+public class QuestionForm {
+    public String question;
+    public String answer;
+
+    public QuestionForm(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
