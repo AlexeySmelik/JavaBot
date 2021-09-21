2a2aedd4e041073872ceaa6c6a7358aa3b90a441
Nina Prokopova <nina30600@gmail.com>
2021-09-21 07:49:40 +0500

    Add class StringStore, refactor class Bot use String Store

---
+++ Bot/src/StringStore.java
@@ -0,0 +1,9 @@
+public class StringStore {
+    public static final String end = "Конец)0))\nПропиши \\restart, чтобы начать заново!";
+    public static final String next = "Next)0))";
+    public static final String wrongAnswer = "Неправильный ответ)0))";
+    public static final String description = "Hello... my description...";
+    public static final String start = "Go!";
+    public static final String help = "Норм бот!";
+    public static final String restart = "Let's go!";
+}
