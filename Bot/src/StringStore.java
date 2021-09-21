cb9a5883473b3968929b22dfea4e15aa4d3a741b
Nina Prokopova <nina30600@gmail.com>
2021-09-21 20:01:57 +0500

    Change StringStore.description

--- Bot/src/StringStore.java
+++ Bot/src/StringStore.java
@@ -2,7 +2,7 @@ public class StringStore {
     public static final String end = "Конец)0))\nПропиши \\restart, чтобы начать заново!";
     public static final String next = "Next)0))";
     public static final String wrongAnswer = "Неправильный ответ)0))";
-    public static final String description = "Hello... my description...";
+    public static final String description = "Hello... Я бот, который задаёт вопросы :) Ты можешь ответить Да, Нет, Возможно. Для начала введи \\start, для завершения \\end, \\restart чтобы начать заново";
     public static final String start = "Go!";
     public static final String help = "Норм бот!";
     public static final String restart = "Let's go!";
