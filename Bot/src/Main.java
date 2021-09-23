197c12ab92f2b8c127f3638df4841d56f7dadef4
Alexey Smelik <aleshabrave@gmail.ru>
2021-09-23 19:57:23 +0500

    Refactor Main.java

--- Bot/src/Main.java
+++ Bot/src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String [] args) {
-        var i = new String("111");
-        var o = (Object) i;
-        System.out.println(o);
         var bot = new Bot();
         bot.startPolling();
     }
