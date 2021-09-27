0a9602ca0feb681a6d3a288057b90bdf169eb6ae
Alexey Smelik <aleshabrave@gmail.ru>
2021-09-27 16:53:06 +0500

    Fix: ContextManager and Context

--- Bot/src/handlers/ContextEventManager.java
+++ Bot/src/handlers/ContextEventManager.java
@@ -1,5 +1,7 @@
 package handlers;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,23 +10,23 @@ import java.util.Map;
 public class ContextEventManager implements EventManager<Context> {
     private final Map<String, List<EventListener>> listeners;
 
-    public ContextEventManager(List<String> operations) {
+    public ContextEventManager(@NotNull List<String> operations) {
         listeners = new HashMap<>();
-        for (String operation : operations)
+        for (var operation : operations)
             listeners.put(operation, new ArrayList<>());
     }
 
-    public void add(String eventType, EventListener listener) {
+    public void add(@NotNull String eventType, @NotNull EventListener listener) {
         List<EventListener> users = listeners.get(eventType);
         users.add(listener);
     }
 
-    public void remove(String eventType, EventListener listener) {
+    public void remove(@NotNull String eventType, @NotNull EventListener listener) {
         List<EventListener> users = listeners.get(eventType);
         users.remove(listener);
     }
 
-    public void notify(String eventType, Context context) {
+    public void notify(@NotNull String eventType, Context context) {
         List<EventListener> users = listeners.get(eventType);
         for (EventListener listener : users)
             listener.update(eventType, context);
