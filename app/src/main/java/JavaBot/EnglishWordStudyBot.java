9ab25a806df17f333ad48733985a8686fe0c4aaf
PeterMotorniy <motorniy.peter@gmail.com>
2021-10-18 21:42:51 +0500

    Changed dialog for Telegram

--- app/src/main/java/JavaBot/EnglishWordStudyBot.java
+++ app/src/main/java/JavaBot/EnglishWordStudyBot.java
@@ -7,12 +7,14 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import com.google.gson.Gson;
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class EnglishWordStudyBot extends TelegramLongPollingBot {
     private Config config;
-
+    private OldBot old;
+    private String ChatId;
     public EnglishWordStudyBot() {
         super();
         var configURL = ClassLoader.getSystemClassLoader().getResource("config.json");
@@ -22,8 +24,20 @@ public class EnglishWordStudyBot extends TelegramLongPollingBot {
             var file = new File(configURL.toURI());
             var lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
             config = gson.fromJson(String.join("", lines), Config.class);
+            old = new OldBot(this);
+
         } catch (Exception e) {
             config = null;
+            e.printStackTrace();
+        }
+    }
+    public void print(String text) {
+        var message = new SendMessage();
+        message.setChatId(ChatId);
+        message.setText(text);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
             e.printStackTrace();
         }
     }
@@ -41,15 +55,8 @@ public class EnglishWordStudyBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            var message = new SendMessage();
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
-
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+            ChatId = update.getMessage().getChatId().toString();
+            old.context.updateMessage(update.getMessage().getText());
         }
     }
 }
